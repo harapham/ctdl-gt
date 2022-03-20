7e26ec38bcc6f67dfaf730c840b1b339762ec7dc
harapham <thanhtrang.230402@gmail.com>
2022-03-20 16:18:38 +0700

    hara

--- DSA05013.cpp
+++ DSA05013.cpp
@@ -1,2 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
+int m=1e9+7;
+long long tinh(int n,int k){
+    long long f[100005]={0};
+    f[0]=1;f[1]=1;
+    for(int i=2;i<=n;i++){
+        for(int j=1;j<=min(i,k);j++){
+            f[i]=(f[i]+f[i-j])%m;
+        }
+    }
+    return f[n];
+}
+main(){
+    int t;cin>>t;
+    while(t--){
+        int n,k;
+        cin>>n>>k;
+        cout<<tinh(n,k)<<endl;
+    }
+}
